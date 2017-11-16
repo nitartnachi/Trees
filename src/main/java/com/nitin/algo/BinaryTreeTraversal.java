b5aabf1ff1c264efa468e2ac83573e13b156d644
Nitin Sharma <nsharma78@gmail.com>
2017-11-15 18:42:17 -0800

    Creation and Traversla of tree

--- src/main/java/com/nitin/algo/BinaryTreeTraversal.java
+++ src/main/java/com/nitin/algo/BinaryTreeTraversal.java
@@ -9,8 +9,43 @@ package com.nitin.algo;
 
 public class BinaryTreeTraversal {
 	
-	public static void main(String[] args) {
+	private static void preOrderTraversal(Node node) {
+		if(node == null)
+			return;
+		System.out.println(node.getKey());
+		preOrderTraversal(node.getLeft());
+		preOrderTraversal(node.getRight());
 		
+	}
+	
+	private static void inOrderTraversal(Node node) {
+		if(node == null)
+			return;
+		inOrderTraversal(node.getLeft());
+		System.out.println(node.getKey());
+		inOrderTraversal(node.getRight());
+		
+	}
+	
+	private static void postOrderTraversal(Node node) {
+		if(node == null)
+			return;
+		postOrderTraversal(node.getLeft());
+		postOrderTraversal(node.getRight());
+		System.out.println(node.getKey());
+		
+	}
+	
+	public static void main(String[] args) {
+		Node root = BinaryTree.getBinaryTree();
+		System.out.println("=====PreOrder=====");
+		preOrderTraversal(root);
+		
+		System.out.println("=====InOrder=====");
+		inOrderTraversal(root);
+		
+		System.out.println("=====PostOrder=====");
+		postOrderTraversal(root);
 	}
 
 }
