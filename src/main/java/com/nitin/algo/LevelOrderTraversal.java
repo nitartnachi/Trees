069cef1617b2553809d7d1ac819ccd1408666352
Nitin Sharma <nsharma78@gmail.com>
2017-11-22 15:32:28 -0800

    LevelOrderTraversal implemented

---
+++ src/main/java/com/nitin/algo/LevelOrderTraversal.java
@@ -0,0 +1,66 @@
+/*
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+For example:
+Given binary tree {3,9,20,23,14,15,7},
+ * 			3
+ * 		   / \
+ * 		9/     \20
+ *    23/\14 15/ \7
+return its level order traversal as [[3], [9,20], [23,14,15,7]]
+ * ----------------------------------------------------------------------------------------------------------------------------------------------
+ * BIG O Details: time O(n) -- space O(1)
+ */
+
+package com.nitin.algo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import com.nitin.algo.TreeUtils.Node;
+
+public class LevelOrderTraversal {
+	
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,4,5,6,7,8,9};
+		Node root = TreeUtils.sortedArrayToBST(arr, 0, arr.length - 1);
+		System.out.println("========LevelOrderTraversalForBST=========");
+		List<List<Integer>> list = levelOrderTraversal(root);
+		System.out.println(list.toString());
+		
+		System.out.println("========LevelOrderTraversalForInsertLevelOrder=========");
+		root = TreeUtils.insertLevelOrder(arr, null, 0);
+		list = levelOrderTraversal(root);
+		System.out.println(list.toString());
+	}
+
+	private static List<List<Integer>> levelOrderTraversal(Node root) {
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+		
+		Queue<Node> queue = new LinkedList<>();
+		Node temp = null;
+		int size = 0;
+		if(root != null)
+			queue.add(root);
+		
+		while(!queue.isEmpty()) {
+			List<Integer> list = new ArrayList<>();
+			size = queue.size();
+			while (size-- > 0) {
+				temp = queue.remove();
+				list.add(temp.data);
+				if(temp.left != null)
+					queue.add(temp.left);
+				if(temp.right != null)
+					queue.add(temp.right);
+			}
+			result.add(list);
+			
+		}
+			
+		return result;
+	}
+
+}
