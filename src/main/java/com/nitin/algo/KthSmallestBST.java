e6c70fe2aecad8d7b9e70e3dcde983973d4783c8
Nitin Sharma <nsharma78@gmail.com>
2017-11-22 19:10:36 -0800

    KthSmallestBST implemented

---
+++ src/main/java/com/nitin/algo/KthSmallestBST.java
@@ -0,0 +1,47 @@
+/*
+ * Given a binary search tree, write a function kthSmallest to find the kth smallest element in it. (1 ≤ k ≤ BST's total elements)
+ */
+
+package com.nitin.algo;
+
+import java.util.Stack;
+
+import com.nitin.algo.TreeUtils.Node;
+
+public class KthSmallestBST {
+	
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,4,5,6,7,8,9};
+		Node root = TreeUtils.sortedArrayToBST(arr, 0, arr.length - 1);
+		int k = 8;
+		System.out.println("The Kth smallest in the give BST is: "  + kthSmallest(root, k));
+	}
+
+	private static int kthSmallest(Node root, int k) {
+		int result = Integer.MIN_VALUE;
+		if(root == null)
+			return result;
+		
+		Stack<Node> stk = new Stack<>();
+		Node node = root;
+		
+		while(node != null || !stk.isEmpty()) {
+			
+			if(node != null) {
+				stk.push(node);
+				node = node.left;
+				
+			}else {
+				Node temp = stk.pop();
+				k--;
+				if(k == 0)
+					result = temp.data;
+				node = temp.right;
+			}
+			
+		}
+
+		return result;
+	}
+
+}
