eeb613317e102eb3214562a9a05b541f23a11c85
Nitin Sharma <nsharma78@gmail.com>
2018-01-04 13:56:08 -0800

    LevelOrderAverages implemented

---
+++ src/main/java/com/nitin/algo/LevelOrderAverages.java
@@ -0,0 +1,52 @@
+package com.nitin.algo;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import com.nitin.algo.TreeUtils.Node;
+
+public class LevelOrderAverages {
+	
+	public static void main(String[] args) {
+		int[] arr = {3,9,20,15,7};
+		
+		System.out.println("========LevelOrderTraversalForInsertLevelOrder=========");
+		Node root = TreeUtils.insertLevelOrder(arr, null, 0);
+		List<Double> list = levelOrderAverages(root);
+		System.out.println(list.toString());
+	}
+
+	public static List<Double> levelOrderAverages(Node root) {
+		List<Double> result = new ArrayList<Double>();
+		
+		Queue<Node> queue = new LinkedList<>();
+		Node temp = null;
+		int size = 0;
+		if(root != null)
+			queue.add(root);
+		
+		while(!queue.isEmpty()) {
+			List<Integer> list = new ArrayList<>();
+			size = queue.size();
+			while (size-- > 0) {
+				temp = queue.remove();
+				list.add(temp.data);
+				if(temp.left != null)
+					queue.add(temp.left);
+				if(temp.right != null)
+					queue.add(temp.right);
+			}
+			double sum = 0;
+			for(int i = 0; i < list.size(); i++) {
+				sum = sum + list.get(i);
+			}
+			result.add(sum/(list.size()));
+			
+		}
+			
+		return result;
+	}
+
+}
