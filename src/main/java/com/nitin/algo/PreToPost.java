e9a5ba59122c457035c6e9fcd51b2bd127620116
Nitin Sharma <nsharma78@gmail.com>
2018-01-13 17:21:35 -0800

    ZigZagTraversal implemented

---
+++ src/main/java/com/nitin/algo/PreToPost.java
@@ -0,0 +1,18 @@
+/*
+ * Given an array representing preorder traversal of BST, print its postorder traversal.
+ * Examples
+ * Pre = {40,30,35,80,100}
+ * Post = {35,30,100,80,40}
+ * 
+ * Pre = {40,30,32,35,80,90,100,120}
+ * Post = {35,32,30,120,100,90,80,40}
+ * 
+ * Pre = {7,9,6,1,4,2,3,40}
+ * Post = Not Possible
+ */
+
+package com.nitin.algo;
+
+public class PreToPost {
+
+}
