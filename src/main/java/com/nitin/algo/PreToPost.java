3e0157073f6939b95c263d09af1a7d3fe9c8f0d2
Nitin Sharma <nsharma78@gmail.com>
2018-01-14 15:16:50 -0800

    PreToPost implemented

--- src/main/java/com/nitin/algo/PreToPost.java
+++ src/main/java/com/nitin/algo/PreToPost.java
@@ -13,6 +13,58 @@
 
 package com.nitin.algo;
 
+import com.nitin.algo.TreeUtils.Node;
+
 public class PreToPost {
+
+	private static int preIndex;
+
+	private static Node constructTreeUtil(int pre[], int key,
+			int min, int max) {
+
+		// Base case
+		if (preIndex >= pre.length) {
+			return null;
+		}
+
+		Node root = null;
+
+		// If current element of pre[] is in range, then
+		// only it is part of current subtree
+		if (key > min && key < max) {
+
+			// Allocate memory for root of this subtree and increment *preIndex
+			root = new Node(key);
+			preIndex++;
+
+			if (preIndex < pre.length) {
+
+				// Contruct the subtree under root
+				// All nodes which are in range {min .. key} will go in left
+				// subtree, and first such node will be root of left subtree.
+				root.left = constructTreeUtil(pre, pre[preIndex], min, key);
+
+				// All nodes which are in range {key..max} will go in right
+				// subtree, and first such node will be root of right subtree.
+				root.right = constructTreeUtil(pre, pre[preIndex], key, max);
+			}
+		}
+
+		return root;
+	}
+
+	private static Node constructTree(int pre[], int size) {
+		preIndex = 0;
+		return constructTreeUtil(pre, pre[0], Integer.MIN_VALUE,
+				Integer.MAX_VALUE);
+	}
+
+	public static void main(String[] args) {
+
+		int pre[] = new int[]{40,30,35,80,100};
+		Node root = constructTree(pre, pre.length);
+		System.out.println("PostOrder traversal of the constructed tree is ");
+		PreOrderInOrderPostOrderTraversal.postOrderTraversal(root);
+	}
 
 }
